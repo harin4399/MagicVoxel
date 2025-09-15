ae0e088063362600550e38fa7988940ab709af42
ybsakura0104 <ybsakura0104@naver.com>
2025-09-15 10:56:23 +0900

    오브젝트 풀 사용하기

--- Assets/Scripts/Voxel.cs
+++ Assets/Scripts/Voxel.cs
@@ -12,8 +12,10 @@ public class Voxel : MonoBehaviour
     float currentTime = 0;
 
     // Start is called before the first frame update
-    void Start()
+    void OnEnable()     // OnEnable : 이벤트가 발생했을 때
     {
+        currentTime = 0;
+
         // 랜덤으로 날아갈 방향을 찾는다
         Vector3 direction = Random.insideUnitSphere;    // 크기가 1이고 방향만 존재함
 
@@ -29,7 +31,14 @@ public class Voxel : MonoBehaviour
         currentTime = currentTime + Time.deltaTime;
         if(currentTime > destroyTime)   // 시간초과
         {
-            Destroy(gameObject);    // 복셀 제거
+            gameObject.SetActive(false);    // 복셀을 비활성화
+            
+            // 오브젝트 풀에 다시 넣어준다 (나(gameObject)를 넣어준다)
+            // VoxelMaker의 오브젝트풀에 자기 자신을 추가
+            // 복셀메이커 안에 있는 복셀 풀에 나(게임 오브젝트)를 추가한다
+            VoxelMaker.voxelPool.Add(gameObject);
+
+            // Destroy(gameObject);    // 복셀 제거
         }
     }
 }
--- Assets/Scripts/VoxelMaker.cs
+++ Assets/Scripts/VoxelMaker.cs
@@ -4,32 +4,61 @@ using UnityEngine;
 
 public class VoxelMaker : MonoBehaviour
 {
-    // 복셀 공장장
+    // 복셀 공장
     public GameObject voxelFactory;
+
+    // 오브젝트 풀의 크기
+    public int voxelPoolSize = 20;
+
+    // 오브젝트 풀 - 필요한 리소스를 미리 만들어서 관리 / 로딩하는 시간 단축
+    // static 선언 : 하나만 선언한다 - 모든 복셀들이 하나의 선언을 받는다
+    public static List<GameObject> voxelPool = new List<GameObject>();
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 오브젝트 풀에 비활성화된 복셀을 담고 싶다.
+        for (int i = 0; i < voxelPoolSize; i++)
+        {
+            // 1. 복셀 공장에서 복셀 생성
+            GameObject voxel = Instantiate(voxelFactory);
+
+            // + 복셀에 색상 입히기
+            voxel.Render =
+
+            // 2. 복셀 비활성화
+            voxel.SetActive(false);
+
+            // 3. 복셀을 오브젝트 풀에 담고 싶다.
+            voxelPool.Add(voxel);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         // 사용자가 마우스를 클릭한 지점에 복셀을 1개 만들고 싶다.
-        // 1. 사용자가 마우스를 클릭했다면면
+        // 1. 사용자가 마우스를 클릭했다면
         if (Input.GetButtonDown("Fire1"))
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             // RaycastHit : 
             RaycastHit hitInfo = new RaycastHit();
 
-            // 2. 마우스의 위치가 바닥 위에 위치에 있다면면
+            // 2. 마우스의 위치가 바닥 위에 위치에 있다면
             if (Physics.Raycast(ray, out hitInfo))
             {
-                // 3. 복셀 공장에서 복셀을 만들어야 한다.
-                GameObject voxel = Instantiate(voxelFactory);
-                // 4. 복셀을 배치하고 싶다.
-                voxel.transform.position = hitInfo.point;
+                if (voxelPool.Count > 0)    // 오브젝트 풀 안에 voxel이 있는지 확인
+                {
+                    GameObject voxel = voxelPool[0];    // 오브젝트 풀 최상단의 값을 가져옴
+
+                    voxel.SetActive(true);  // 복셀을 활성화
+
+                    voxel.transform.position = hitInfo.point;   // RayCast를 통해 얻은 충돌지점의 위치로 객체를 이동
+
+                    voxelPool.RemoveAt(0);  // 오브젝트 풀에서 복셀을 하나 제거
+                }
             }
         }
     }
